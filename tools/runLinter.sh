2c82fb615e9c30d937f538185b55a92d368e549f
Brian Avery <bavery@redhat.com>
2020-04-23 16:25:04 -0400

    Check shell scripts; code review comments

--- tools/runLinter.sh
+++ tools/runLinter.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 set -e -x
+
+#verify shell scripts
+find . -name '*.sh' -print0 | xargs -0 -r shellcheck
+
+#run documentation linting. See: https://github.com/errata-ai/vale
 vale .
--- tools/updateFields.sh
+++ tools/updateFields.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#set -x
+set -e -x
 
 mkdir -p out
 
@@ -7,30 +7,30 @@ getMaistra() {
     maistraBranch=$(yq read data/release.yaml maistraBranch)
     maistraVersion=$(yq read data/release.yaml maistraOperatorVersion)
 
-    wget https://raw.githubusercontent.com/Maistra/istio-operator/${maistraBranch}/resources/smcp-templates/v${maistraVersion}/base -O out/base
-    wget https://raw.githubusercontent.com/Maistra/istio-operator/${maistraBranch}/resources/smcp-templates/v${maistraVersion}/servicemesh -O out/servicemesh
-    wget https://raw.githubusercontent.com/Maistra/istio-operator/${maistraBranch}/resources/smcp-templates/v${maistraVersion}/maistra -O out/maistra
-    yq merge out/base out/maistra > out/maistra.rendered
-    yq merge out/base out/servicemesh > out/servicemesh.rendered
+    wget "https://raw.githubusercontent.com/Maistra/istio-operator/${maistraBranch}/resources/smcp-templates/v${maistraVersion}/base" -O out/base
+    wget "https://raw.githubusercontent.com/Maistra/istio-operator/${maistraBranch}/resources/smcp-templates/v${maistraVersion}/servicemesh" -O out/servicemesh
+    wget "https://raw.githubusercontent.com/Maistra/istio-operator/${maistraBranch}/resources/smcp-templates/v${maistraVersion}/maistra" -O out/maistra
+    yq merge out/base out/maistra > out/maistra.rendered.yaml
+    yq merge out/base out/servicemesh > out/servicemesh.rendered.yaml
 }
 
 getKiali() {
-    file=out/kiali.rendered
+    file=out/kiali.rendered.yaml
 
     wget https://raw.githubusercontent.com/kiali/kiali-operator/master/deploy/kiali/kiali_cr.yaml -O ${file}
 
-    MatchingLines=$(grep -nr "#[[:blank:]]* ---" ${file} | cut -d : -f1 )
+    matchingLines=$(grep -nr "#[[:blank:]]* ---" ${file} | cut -d : -f1 )
 
-    for lineNumber in $MatchingLines; do
+    for lineNumber in $matchingLines; do
         #our line number is the match (---). Skip it.
-        lineNumber=$((${lineNumber}+1))
+        lineNumber=$((lineNumber+1))
 
         #find the next line matching ## or # letter (only one space)
         lineNumberEnd=$(tail -n +$lineNumber ${file} | grep -n -E '##+|#[[:space:]][*a-zA-Z]|#[[:space:]]*---'  | cut -d : -f1 | head -1 )
 
         #decrement linenumberend and combine it with linenumber since it's an offset
-        lineNumberEnd=$((${lineNumber} + ${lineNumberEnd} - 2))
-        if [[ -z $lineNumberEnd ]]; then
+        lineNumberEnd=$((lineNumber + lineNumberEnd - 2))
+        if [ -z $lineNumberEnd ]; then
 			#HACK: this number should be bigger than any length the file might be. It just tells the sed to go past the end of the file.
             lineNumberEnd=5000000
         fi
@@ -40,30 +40,31 @@ getKiali() {
 }
 
 getValue() {
-    yq read out/$1.rendered $2
+    yq read "out/${1}.rendered.yaml" "${2}"
 }
 
 updateValuesFile() {
-    input=$1
+    input="${1}"
     replace=""
     while IFS= read -r line
     do
-        if [[ $replace ]]; then
+        if [ $replace ]; then
             echo "replacing: ${line} with: ${replace}"
             echo ""
 
-            lineValues=($line)
-            argName=${lineValues[0]}
-            sed -i "s#${argName}.*#${argName} ${replace}#" ${input}
+            argName=$(echo "${line}" | cut -d' ' -f1)
+            sed -i "s#${argName}.*#${argName} ${replace}#" "${input}"
 
             replace=""
         fi
-        if [[ $line == *"//Value"* ]]; then
+        
+        #use posix substring to work across shells.
+        #this checks to see if ${line} contains "//Value"
+        if [ "${line#*//Value}" != "${line}" ]; then
             match=$line
-            matchValues=($match)
-            specFile=${matchValues[1]}
-            specPath=${matchValues[2]}
-            replace=$(getValue ${specFile} ${specPath})
+            specFile=$(echo "${match}" | cut -d' ' -f2)
+            specPath=$(echo "${match}" | cut -d' ' -f3)
+            replace=$(getValue "${specFile}" "${specPath}")
             echo "GetValue ${specPath} from ${specFile}. Value: ${replace}"
         fi
     done < "$input"
@@ -72,4 +73,4 @@ updateValuesFile() {
 
 getMaistra
 getKiali
-updateValuesFile $1
+updateValuesFile "${1}"
